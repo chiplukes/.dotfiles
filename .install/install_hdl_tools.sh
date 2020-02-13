abb48a3c7c8295ed43d64f123948427ed1536c64
chiplukes <34627158+chiplukes@users.noreply.github.com>
2020-02-13 10:16:39 -0700

    moved installs to tmp folder, fixed permission issue w/ myhdl

--- .install/install_hdl_tools.sh
+++ .install/install_hdl_tools.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y flex
 sudo apt-get install -y bison
 
 # setting up Icarus Verilog
-cd ~
+cd ~/tmp
 git clone https://github.com/steveicarus/iverilog.git
 cd iverilog
 #git checkout --track -b v10-branch origin/v10-branch
@@ -24,9 +24,9 @@ fi
 
 # Setting up MyHDL
 export PYTHON_EXE=$HOME/.pyenv/versions/general/bin/python
-cd ~
+cd ~/tmp
 git clone https://github.com/jandecaluwe/myhdl.git
-cd ~/myhdl && sudo $PYTHON_EXE setup.py install
-cd ~/myhdl/cosimulation/icarus && make && sudo install -m 0755 -D ./myhdl.vpi /usr/lib/ivl/myhdl.vpi
-sudo cp $HOME/myhdl/cosimulation/icarus/myhdl.vpi /usr/local/lib/ivl
+cd ~/tmp/myhdl && $PYTHON_EXE setup.py install
+cd ~/tmp/myhdl/cosimulation/icarus && make && sudo install -m 0755 -D ./myhdl.vpi /usr/lib/ivl/myhdl.vpi
+sudo cp $HOME/tmp/myhdl/cosimulation/icarus/myhdl.vpi /usr/local/lib/ivl
 
