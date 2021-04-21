563a8d505c36f5db65d95d479bdd231ccbd7a825
Chip Lukes <34627158+chiplukes@users.noreply.github.com>
2021-04-21 12:02:47 -0600

    fixed risc install script

--- .install/install_risc_dev_tools.sh
+++ .install/install_risc_dev_tools.sh
@@ -1,4 +1,24 @@
-# work pc setup
+# riscv gnu toolchain setup
+sudo apt-get install -y autoconf
+sudo apt-get install -y automake
+sudo apt-get install -y autotools-dev
+sudo apt-get install -y curl
+sudo apt-get install -y libmpc-dev
+sudo apt-get install -y libmpfr-dev
+sudo apt-get install -y libgmp-dev
+sudo apt-get install -y gawk
+sudo apt-get install -y build-essential
+sudo apt-get install -y bison
+sudo apt-get install -y flex
+sudo apt-get install -y texinfo
+sudo apt-get install -y gperf
+sudo apt-get install -y libtool
+sudo apt-get install -y patchutils
+sudo apt-get install -y bc
+sudo apt-get install -y zlib1g-dev
+sudo apt-get install -y libexpat-dev
+
+
 mkdir -p ~/tmp
 cd ~/tmp
 git clone --depth=1 git://gcc.gnu.org/git/gcc.git gcc
@@ -14,6 +34,6 @@ cd build
 ../configure --target=riscv32-unknown-elf --enable-languages=c --disable-shared --disable-threads --disable-multilib --disable-gdb --disable-libssp --with-newlib --with-arch=rv32ima --with-abi=ilp32 --prefix=/usr/local/share/gcc-riscv32-unknown-elf
 make -j4
 make
-make install
+sudo make install
 export PATH=$PATH:/usr/local/share/gcc-riscv32-unknown-elf/bin/
 riscv32-unknown-elf-gcc -v
